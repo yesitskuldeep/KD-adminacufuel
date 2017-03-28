22d3192382f19183626d81fdfca84af0b8ba1355
Rishav <rsingla.rishu@gmail.com>
2017-03-28 17:13:03 +0530

    login next page

--- app/views/login/login.controller.js
+++ app/views/login/login.controller.js
@@ -3,15 +3,16 @@
  //Load controller
   angular.module('inspinia')
 
-	.controller('LoginController', function ($scope, LoginService) {
+	.controller('LoginController', function ($scope, $state, LoginService) {
       
       $scope.user = {};
       $scope.userData = function(){
           
-          LoginService.loginUser($scope.user).then(function(result) {
-            console.log(result)
+          // LoginService.loginUser($scope.user).then(function(result) {
+          //   console.log(result)
               
-          })
+          // })
+          $state.go('index.dashboard');
       }
       
   });
