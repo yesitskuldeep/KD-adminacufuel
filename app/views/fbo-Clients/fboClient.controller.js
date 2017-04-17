1c749689b13f274a0e91cc4fa08948ddebcb82c2
Rishav <rsingla.rishu@gmail.com>
2017-04-17 14:45:29 +0530

    search issue solved

--- app/views/fbo-Clients/fboClient.controller.js
+++ app/views/fbo-Clients/fboClient.controller.js
@@ -19,45 +19,45 @@
           })
         }
 
-        $scope.searchData = function(){
-          var userType = $('#tabClient > li.active > a').attr('id');
-          searchClient($scope.searchQuery, userType);
-        }
+        // $scope.searchData = function(){
+        //   var userType = $('#tabClient > li.active > a').attr('id');
+        //   searchClient($scope.searchQuery, userType);
+        // }
         
-        function searchClient(searchData, userType){
-          console.log(userType)
-          if(searchData == null || searchData == undefined || searchData == "" && userType == 'fbo'){
-            getAllFbo();
-          }else if(searchData == null || searchData == undefined || searchData == "" && userType == 'flightDetp'){
-            $scope.fboDept();
-          }else{
-            var data = {
-              "query" : searchData,
-              "userType" : userType
-            }
-            FBOClient.getSearchClient(data).then(function(result) {
-              for(var i=0; i<result.length; i++){
-                result[i].user = [];
-                result[i].user.companyName = result[i].companyName;
-                result[i].user.firstName = result[i].firstName;
-                result[i].user.lastName = result[i].lastName;
-                result[i].user.id = result[i].id;
-                result[i].user.account = [];
-                result[i].user.account.user = [];
-                result[i].user.account.user.status = result[i].account.user.status;
-              }
-              if(userType == 'fbo'){
-                $scope.fboClient = result;
-                console.log(result)
-                $scope.clientLength = result.length;
-              }else{
-                $scope.fboDeptList = result;
-                $scope.clientLength = result.length;
-              }
-            })
-          }
+        // function searchClient(searchData, userType){
+        //   console.log(userType)
+        //   if(searchData == null || searchData == undefined || searchData == "" && userType == 'fbo'){
+        //     getAllFbo();
+        //   }else if(searchData == null || searchData == undefined || searchData == "" && userType == 'flightDetp'){
+        //     $scope.fboDept();
+        //   }else{
+        //     var data = {
+        //       "query" : searchData,
+        //       "userType" : userType
+        //     }
+        //     FBOClient.getSearchClient(data).then(function(result) {
+        //       for(var i=0; i<result.length; i++){
+        //         result[i].user = [];
+        //         result[i].user.companyName = result[i].companyName;
+        //         result[i].user.firstName = result[i].firstName;
+        //         result[i].user.lastName = result[i].lastName;
+        //         result[i].user.id = result[i].id;
+        //         result[i].user.account = [];
+        //         result[i].user.account.user = [];
+        //         result[i].user.account.user.status = result[i].account.user.status;
+        //       }
+        //       if(userType == 'fbo'){
+        //         $scope.fboClient = result;
+        //         console.log(result)
+        //         $scope.clientLength = result.length;
+        //       }else{
+        //         $scope.fboDeptList = result;
+        //         $scope.clientLength = result.length;
+        //       }
+        //     })
+        //   }
           
-        }
+        // }
 
       	$scope.fboAdmin = function(){
           getAllFbo();
--- app/views/flightDept/flightDeptController.js
+++ app/views/flightDept/flightDeptController.js
@@ -24,46 +24,47 @@
           })
         }
 
-        $scope.searchData = function(){
-          var userType = $('#tabClient > li.active > a').attr('id');
-          searchClient($scope.searchQuery, userType);
-        }
+        // $scope.searchData = function(){
+        //   console.log($scope.searchQuery)
+        //   var userType = $('#tabClient > li.active > a').attr('id');
+        //   searchClient($scope.searchQuery, userType);
+        // }
         
-        function searchClient(searchData, userType){
-          console.log(searchData)
-          console.log(userType)
-          if(searchData == null || searchData == undefined || searchData == "" && userType == 'fbo'){
-            $scope.fboAdmin();
-          }else if(searchData == null || searchData == undefined || searchData == "" && userType == 'flightDetp'){
-            getAllFbo();
-          }else{
-            var data = {
-              "query" : searchData,
-              "userType" : userType
-            }
-            FBOClient.getSearchClient(data).then(function(result) {
-              for(var i=0; i<result.length; i++){
-                result[i].user = [];
-                result[i].user.companyName = result[i].companyName;
-                result[i].user.firstName = result[i].firstName;
-                result[i].user.lastName = result[i].lastName;
-                result[i].user.id = result[i].id;
-                result[i].user.account = [];
-                result[i].user.account.user = [];
-                result[i].user.account.user.status = result[i].account.user.status;
-              }
-              if(userType == 'fbo'){
-                $scope.fboClient = result;
-                console.log(result)
-                $scope.clientLength = result.length;
-              }else{
-                console.log("nai aaya")
-                $scope.fboDeptList = result;
-                $scope.clientLength = result.length;
-              }
-            })
-          }
-        }
+        // function searchClient(searchData, userType){
+        //   console.log(searchData)
+        //   console.log(userType)
+        //   if(searchData == null || searchData == undefined || searchData == "" && userType == 'fbo'){
+        //     $scope.fboAdmin();
+        //   }else if(searchData == null || searchData == undefined || searchData == "" && userType == 'flightDetp'){
+        //     getAllFbo();
+        //   }else{
+        //     var data = {
+        //       "query" : searchData,
+        //       "userType" : userType
+        //     }
+        //     FBOClient.getSearchClient(data).then(function(result) {
+        //       for(var i=0; i<result.length; i++){
+        //         result[i].user = [];
+        //         result[i].user.companyName = result[i].companyName;
+        //         result[i].user.firstName = result[i].firstName;
+        //         result[i].user.lastName = result[i].lastName;
+        //         result[i].user.id = result[i].id;
+        //         result[i].user.account = [];
+        //         result[i].user.account.user = [];
+        //         result[i].user.account.user.status = result[i].account.user.status;
+        //       }
+        //       if(userType == 'fbo'){
+        //         $scope.fboClient = result;
+        //         console.log(result)
+        //         $scope.clientLength = result.length;
+        //       }else{
+        //         console.log("nai aaya")
+        //         $scope.fboDeptList = result;
+        //         $scope.clientLength = result.length;
+        //       }
+        //     })
+        //   }
+        // }
 
       	$scope.fboAdmin = function(){
           FBOClient.getALlFBOList().then(function(result) {
